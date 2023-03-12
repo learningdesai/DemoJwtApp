44bc26334624fa464cc6ecf8483861aab15eee86
learningdesai <learningdesai@gmail.com>
2023-03-12 17:00:04 +0530

    Distributed Cache implementation on WeatherForeCast ontroller

--- DemoJwtApp/Controllers/WeatherForecastController.cs
+++ DemoJwtApp/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 
 namespace DemoJwtApp.Controllers
 {
@@ -6,28 +8,52 @@ namespace DemoJwtApp.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private IDistributedCache _distributedCache;
+
+        public WeatherForecastController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        //private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+        //public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IEnumerable<WeatherForecast>> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var tomorrow=DateTime.Now.Date.AddDays(1); ;
+            var totalSeconds=tomorrow.Subtract(DateTime.Now).TotalSeconds;
+
+            var DCEntryOptions = new DistributedCacheEntryOptions();
+            DCEntryOptions.AbsoluteExpirationRelativeToNow=TimeSpan.FromSeconds(totalSeconds);
+            DCEntryOptions.SlidingExpiration = null;
+
+            IEnumerable<WeatherForecast> forecasts = new List<WeatherForecast>();
+ 
+
+
+            forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            });
+
+            var jsonData=JsonConvert.SerializeObject(forecasts);
+
+            await _distributedCache.SetStringAsync("ForeCast",jsonData,DCEntryOptions);
+
+            return forecasts.ToArray();
+
         }
     }
 }
--- DemoJwtApp/Program.cs
+++ DemoJwtApp/Program.cs
@@ -1,12 +1,19 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddStackExchangeRedisCache(options => {
+    string connection = builder.Configuration.GetSection("RedisConnection:Configuration").Value;
+    string instancename = builder.Configuration.GetSection("RedisConnection:RedisDemo").Value;
+    options.Configuration = connection;
+    options.InstanceName = instancename;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,7 +22,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
