c8c91ac8ce4ad002fc26d2a0e8634eb8389014a3
learningdesai <learningdesai@gmail.com>
2023-03-12 12:41:20 +0530

    Add project files.

---
+++ DemoJwtApp/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoJwtApp
+{
+    public class UserDto
+    {
+        //user datat transfer object (DTO)
+        public string Username { get; set; }= string.Empty;
+        public string Password { get; set; }=string.Empty;
+    }
+}
